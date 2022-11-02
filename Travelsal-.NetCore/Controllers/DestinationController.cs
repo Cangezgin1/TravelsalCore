fa880b9a669cdf901d29c42efa1338efd67ff314
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2022-11-02 11:32:01 +0300

    First Comiit
    Daha önce üstünde çalışmış olduğum Traversal Seyehat Projesi için tekrar Repository oluşturmak zorunda kaldım.

---
+++ Travelsal-.NetCore/Controllers/DestinationController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Travelsal_.NetCore.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public IActionResult DestinotionDetails(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinotionDetails(Destination p)
+        {
+            return View();
+        }
+    }
+}
